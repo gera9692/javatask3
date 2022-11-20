ff41d69631b242852c409a5feda20edca9f14afa
gerald manyasa <93513811+gera9692@users.noreply.github.com>
2022-11-20 14:48:29 +0300

    Create mutableclass.java

---
+++ mutableclass.java
@@ -0,0 +1,15 @@
+public class IntegerPair {
+    int numx;
+    int numy;
+
+    IntegerPair(int x, int y) {
+        this.numx = numx;
+        this.numy =numy;
+    }
+}
+
+IntegerPair pan = new IntegerPair(5, 10);
+// pan.numx = 5, pan.numy = 10
+
+pan.numx = 50;
+// pan.numx = 50, pan.numy = 10
